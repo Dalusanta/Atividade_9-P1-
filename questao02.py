567578a3aa189c723a7db675dc83c469ec19dc66
Dalusanta <dalusantac@gmail.com>
2025-07-06 14:24:11 -0300

    S

---
+++ questao02.py
@@ -0,0 +1,5 @@
+def saudacao(nome):
+	print(f'Olá, {nome}!')
+	
+a = input('Qual o seu nome? ')
+saudacao(a)
---
+++ questao03.py
@@ -0,0 +1,6 @@
+def dobro(num):
+	return num*2
+
+a = int(input('Digite um número: '))
+
+print(f'O dobro de {a} é {dobro(a)}.')
